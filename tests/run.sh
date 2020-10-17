eb6cda5c63644c74f64ab71d8915edd4b54e815e
Daniel Shimon <daielk09@gmail.com>
2020-10-18 00:18:37 +0300

    tests: Remove custom dockerfile

--- tests/run.sh
+++ tests/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 eval $(docker-machine env --shell bash)
-docker run --privileged --rm -t -v "/$PWD":/rpp rpp bash -c \
-"cd build && cmake /rpp  && make && \
-PYTHONPATH=/rpp python /rpp/tests/run_tests.py && \
-PYTHONPATH=/rpp python /rpp/tests/run_benchmarks.py"
+docker run --rm -v "/$PWD":/rpp rikorose/gcc-cmake bash -c \
+    "mkdir build && cd build && cmake /rpp && make && cp rpp /rpp"
+docker run --rm -v "/$PWD":/rpp python bash -c \
+    "cd rpp && PYTHONPATH=/rpp python /rpp/tests/run_tests.py"
